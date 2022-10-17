66fed8578ce735c08320e0ec0d1b3c4af73c0f08
zpfh4664 <93589358+zpfh4664@users.noreply.github.com>
2022-10-17 18:44:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -145,34 +145,12 @@ def send_message(to_user, access_token):
                 "value": "{} {}".format(today, week),
                 "color": get_color()
             },
-            "region": {
-                "value": region_name,
-                "color": get_color()
-            },
-            "weather": {
-                "value": weather,
-                "color": get_color()
-            },
-            "temp": {
-                "value": temp,
-                "color": get_color()
-            },
-            "wind_dir": {
-                "value": wind_dir,
-                "color": get_color()
-            },
             "love_day": {
                 "value": love_days,
                 "color": get_color()
             },
-            "note_en": {
-                "value": note_en,
-                "color": get_color()
-            },
-            "note_ch": {
-                "value": note_ch,
-                "color": get_color()
-            }
+          
+          
         }
     }
     for key, value in birthdays.items():
