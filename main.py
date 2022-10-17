94e04889835414140c8b1d9f0d489a49ca3eafdb
zpfh4664 <93589358+zpfh4664@users.noreply.github.com>
2022-10-17 18:07:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ if __name__ == "__main__":
   
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
